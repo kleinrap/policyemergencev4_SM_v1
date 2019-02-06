47d30fe8332cabcda14731fa2fa41f539792a36a
Raphael <raphael.klein4573@gmail.com>
2019-02-06 15:27:35 +0100

    Update to schelling model

--- code_doc/README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at http://codepen.io/iaian/pen/HElro.
-
- a jqeury implementation of this idea : http://code.stephenmorley.org/javascript/collapsible-lists/
---
+++ run_schelling.py
@@ -0,0 +1,8 @@
+from model_schelling import Schelling
+
+model_run = Schelling(20, 20, 0.8, 0.2, 3)
+
+for i in range(1000):
+	if model_run.happy != model_run.schedule.get_agent_count():
+		print("Step: ", i)
+		model_run.step()
