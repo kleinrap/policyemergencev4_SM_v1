a76608c7be14ace802e5138dcc145bf37c13f361
Raphael <raphael.klein4573@gmail.com>
2019-02-07 16:37:27 +0100

    Addition of happy algorithm and others

--- model_schelling.py
+++ model_schelling.py
@@ -6,12 +6,25 @@ from mesa.datacollection import DataCollector
 '''
 Need addition model - For coupling:
 - Migration
-- Homophily differences between colours
-- Change in the happiness check
-- Movement quota parameter
-- Introduction of the algorithm for moving
-- Additions of the other KPIs
-- 
+    - The net amount of agent change per step needs to be zero.
+    - Random agents are removed? Or unhappy agents? It should be unhappy ones - that makes more sense at least.
+    - Type 0 agents leaving must be replaced by type 1 agents.
+    - Type 1 agents leaving must be replaced by type 0 agents.
+    - Where are new agents incoming placed onto the map? Random locations
+    - Is there a constant turnover? Or is it a one time thing? Dor policy instruments, it makes more sense to have a continuous turnover of a certain percentage
+- Change the movement check
+    - Use a radius to check where to move
+
+
+KPIs:
+What needs to be recorded (KPIs):
+Empty cells -- Implemented
+Green cells (type 0) -- Implemented
+Blue cells (type 1) -- Implemented
+Vision - Global - Not implemented yet
+Movement - Global - Implemented
+Green homophily - Implemented
+Blue homophily - Implemented
 '''
 
 class SchellingAgent(Agent):
@@ -32,29 +45,57 @@ class SchellingAgent(Agent):
 
     def step(self):
 
-        # Happiness check:
-        similar = 0
-        for neighbor in self.model.grid.neighbor_iter(self.pos):
-            if neighbor.type == self.type:
-                similar += 1
+        # Checking if the agent is happy
+        happyBool = self.happy_check()
 
-        # If unhappy, move (only within the quota):
-        if similar < self.model.homophily:
-            # print(self.model.movementQuotaCount, self.model.movementQuota*self.model.schedule.get_agent_count())
+        # If unhappy, move - considering the movement quota and whether the agent is type0 or type1:
+        if self.type == 0 and happyBool == False:
             if self.model.movementQuotaCount <= self.model.movementQuota*self.model.schedule.get_agent_count():
                 self.model.grid.move_to_empty(self)
                 self.model.movementQuotaCount += 1
+        if self.type == 1 and happyBool == False:
+            if self.model.movementQuotaCount <= self.model.movementQuota*self.model.schedule.get_agent_count():
+                self.model.grid.move_to_empty(self)
+                self.model.movementQuotaCount += 1
+
+    def happy_check(self):
+
+        '''
+        Function used to check if the agent is happy in its current position. Use for checking if there is a need to move and to check if the location it is moving to is appropriate.
+        '''
+        # Happiness check:
+        similar = 0
+        happyBool = bool()
+
+        # Finding the neighbors
+        neighborList = self.model.grid.get_neighbors(self.pos, True, False, self.model.happyCheckRadius)
+
+        # Iterating through the neighbors to find whether they are similar:
+        for neighbor in neighborList:
+            if neighbor.type == self.type:
+                similar += 1
+
+        # Converting similarity value into a percentage
+        similar = similar/len(neighborList)
+
+        if self.type == 0 and similar < self.model.homophilyType0:
+            happyBool = False
+        if self.type == 1 and similar < self.model.homophilyType1:
+            happyBool = False
         else:
+            happyBool = True
             self.model.happy += 1
 
+        return happyBool
 
 
 class Schelling(Model):
     '''
     Model class for the Schelling segregation model.
+    This class has been modified from the original model of mesa. Complexity has been added such that the model be used with a model of the policy making process.
     '''
 
-    def __init__(self, height=20, width=20, density=0.8, minority_pc=0.2, homophily=3, movementQuota=0.30):
+    def __init__(self, height=20, width=20, density=0.8, minority_pc=0.2, homophilyType0=0.3, homophilyType1=0.3, movementQuota=0.30, happyCheckRadius=5, moveCheckRadius=5):
         '''
         '''
 
@@ -62,8 +103,11 @@ class Schelling(Model):
         self.width = width
         self.density = density
         self.minority_pc = minority_pc
-        self.homophily = homophily
+        self.homophilyType0 = homophilyType0
+        self.homophilyType1 = homophilyType1
         self.movementQuota = movementQuota
+        self.happyCheckRadius = happyCheckRadius
+        self.moveCheckRadius = moveCheckRadius
 
         self.schedule = RandomActivation(self)
         self.grid = SingleGrid(height, width, torus=True)
@@ -116,7 +160,7 @@ class Schelling(Model):
 
         self.schedule.step()
         print(self.movementQuotaCount, " agents moved.")
-        print(self.happy, " are happy agents.")
+        print(round(self.happy/self.schedule.get_agent_count() * 100,2), " are happy agents.")
 
         # Calculating empty counter
         self.empty = (self.height*self.width) - self.schedule.get_agent_count()
@@ -131,19 +175,6 @@ class Schelling(Model):
         # collect data
         self.datacollector.collect(self)
 
-        # print('self.schedule.get_agent_count(): ', self.schedule.get_agent_count())
-        # print('self.happy: ', self.happy)
-
         if self.happy == self.schedule.get_agent_count():
             self.running = False
-
-
-    # What needs to be recorded (KPIs):
-    # Empty cells -- Total cells - agents number
-    # Green cells (type 0) -- 
-    # Blue cells (type 1) --
-    # Vision - Global - Not implemented yet
-    # Movement - Global - Not implemented yet
-    # Green homophily - Not implemented yet
-    # Blue homophily - Not implemented yet
-
+            print("All agents are happy, the simulation ends!")
--- run_schelling.py
+++ run_schelling.py
@@ -1,6 +1,6 @@
 from model_schelling import Schelling
 
-model_run = Schelling(20, 20, 0.8, 0.2, 3)
+model_run = Schelling(20, 20, 0.8, 0.2)
 
 for i in range(500):
 	if model_run.happy != model_run.schedule.get_agent_count():
